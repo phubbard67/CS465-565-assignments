11f40c3f1020ac936f5a4028ee2a7da27932c86e
phubbard67 <phubbard@pdx.edu>
2019-11-16 13:40:29 -0800

    Finished with as5

--- assignment5/app.js
+++ assignment5/app.js
@@ -4,13 +4,18 @@ const app = express();
 const port = 3000;
 var path = require('path');
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', function(request, response){
     response.sendFile(path.join(__dirname + '/index.html'));
 });
 app.post('/submit', function(req, res) {
-    console.log("We did it");
+    let name = req.body['name'];
+    let email = req.body['email'];
+    let message = req.body['message'];
+    let toSend = 'name: ' + name + '<br> email: ' + email + '<br> comment: ' + message;
+
+    res.send(toSend);
 });
 
 
